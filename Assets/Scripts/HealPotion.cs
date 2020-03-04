c2dcbc5188e664e24a3380255a0b2250f931e224
Mikhail Klishevich <x-ray@mail.ru>
2020-03-04 20:08:41 +0700

    Dog, Animations and Potion
    Dog, with two points of movement
    Layer overlay
    Spawn of coins on the minotaur
    Heal Potion
    Particle System for heal Potion
    Animations for Dog, Coin and Potions

--- Assets/Scripts/HealPotion.cs
+++ Assets/Scripts/HealPotion.cs
@@ -4,28 +4,15 @@ using UnityEngine;
 
 public class HealPotion : MonoBehaviour
 {
-    [SerializeField] int _hpAmount;
-    [SerializeField] private Animator _animator;
-
-    private bool _used = false;
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Start is called before the first frame update
+    void Start()
     {
-        if (collision.TryGetComponent(out Player _) && GameManager.Instance.healthContainer.ContainsKey(collision.gameObject) && !_used)
-        {
-            GameManager.Instance.healthContainer[collision.gameObject].AddHealth(_hpAmount);
-            Destroy();
-        }
+        
     }
 
-    public void Destroy()
+    // Update is called once per frame
+    void Update()
     {
-        _animator.SetTrigger("StartDestroy");
-        _used = true;
-    }
-
-    private void EndDestroy()
-    {
-        Destroy(gameObject);
+        
     }
 }
