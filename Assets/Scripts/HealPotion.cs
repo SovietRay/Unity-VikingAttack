79039d3178cafb97ea8b73446541839ca79756e2
Mikhail Klishevich <x-ray@mail.ru>
2020-03-04 20:17:17 +0700

    Revert "Dog, Animations and Potion"

--- Assets/Scripts/HealPotion.cs
+++ Assets/Scripts/HealPotion.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class HealPotion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int _hpAmount;
+    [SerializeField] private Animator _animator;
+
+    private bool _used = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.TryGetComponent(out Player _) && GameManager.Instance.healthContainer.ContainsKey(collision.gameObject) && !_used)
+        {
+            GameManager.Instance.healthContainer[collision.gameObject].AddHealth(_hpAmount);
+            Destroy();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Destroy()
     {
-        
+        _animator.SetTrigger("StartDestroy");
+        _used = true;
+    }
+
+    private void EndDestroy()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private int _health;
+    [SerializeField] private int _maxHealth;
     [SerializeField] private int _currentHealth;
+    [SerializeField] private GameObject _coinPrefab;
 
+    private void OnValidate()
+    {
+        if (_maxHealth < _currentHealth)
+            _maxHealth = _currentHealth;
+    }
     private void Start()
     {
         GameManager.Instance.healthContainer.Add(gameObject, this);
@@ -17,16 +23,20 @@ public class Health : MonoBehaviour
     }
     public void TakeDamage(int damage)
     {
-        _currentHealth = Mathf.Clamp(_currentHealth - damage, 0, _health);
+        _currentHealth = Mathf.Clamp(_currentHealth - damage, 0, _maxHealth);
         //Debug.Log($"My name - {gameObject.name}, i take {damage} damage! My current health {_currentHealth} from {_health}.");
         if (_currentHealth <= 0)
+        {
+            if (_coinPrefab != null)
+                Instantiate(_coinPrefab, this.transform.position, Quaternion.identity);
             Destroy(gameObject);
+        }
     }
 
     public void AddHealth(int health)
     {
         if (health >= 0)
-            _currentHealth = Mathf.Clamp(_currentHealth + health, 0, _health);
+            _currentHealth = Mathf.Clamp(_currentHealth + health, 0, _maxHealth);
         //Debug.Log($"My name - {gameObject.name}, i take {health} health! My current health {_currentHealth} from {_health}.");
     }
 }
