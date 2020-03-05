ff3c4f5288691fcd1dd570f47f68ed6703dcdea0
Mikhail Klishevich <x-ray@mail.ru>
2020-03-05 23:20:56 +0700

    Fix code and de
    Removed garbage in the code.
    Added removal of the parent in coins and heals.
    Moved part of the code to methods.

--- Assets/Scripts/CollisionDamage.cs
+++ Assets/Scripts/CollisionDamage.cs
@@ -5,19 +5,10 @@ using UnityEngine;
 public class CollisionDamage : MonoBehaviour
 {
     [SerializeField] private int _damage;
-    private void OnParticleCollision(GameObject other)
-    {
-        //SetDamage(other.);
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        
-    }
 
     public void SetDamage(GameObject other)
     {
         if (GameManager.Instance.healthContainer.ContainsKey(other))
             GameManager.Instance.healthContainer[other].TakeDamage(_damage);
     }
-    
 }
---
+++ Assets/Scripts/Enemy/EnemyMeleeAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMeleeAttack : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Transform _attackPoint;
+    [SerializeField] private float _reloadTime = 0.7f;
+    [SerializeField] private int _damage;
+    [SerializeField] private LayerMask _attackMask;
+    [SerializeField] private float _attackRange;
+    public float AttackRange => _attackRange;
+
+    private bool _canHit = true;
+
+    void Update()
+    {
+        if (_canHit)
+            StartCoroutine(Attack());
+    }
+    private IEnumerator Attack()
+    {
+        Collider2D[] hitObject = Physics2D.OverlapCircleAll(_attackPoint.position, _attackRange, _attackMask);
+        foreach (Collider2D obj in hitObject)
+        {
+            if (GameManager.Instance.healthContainer.ContainsKey(obj.gameObject))
+            {
+                _animator.SetTrigger("Attack");
+                GameManager.Instance.healthContainer[obj.gameObject].TakeDamage(_damage);
+                _canHit = false;
+                yield return new WaitForSeconds(_reloadTime);
+                _canHit = true;
+            }
+        }
+        yield return null;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (_attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(_attackPoint.position, _attackRange);
+    }
+}
